fa8f3ff4f1e16656836f19e5882d836b3874ae1e
Vivek Sreekumar <Enzen@Ulf-Hrafn.local>
2023-10-14 10:49:35 +0530

    Learning Array Methods

--- JS 111023.js
+++ JS 111023.js
@@ -1,29 +1,35 @@
 //Array work
-let favouriteFoods = ["chicken", "brocolli", "rice"];
-console.log (favouriteFoods[1]);
-favouriteFoods.splice(2,1,"biryani");
-console.log(favouriteFoods);
-let newJunkFood = favouriteFoods.shift();
-console.log(newJunkFood)
-favouriteFoods.push("Whey Protein");
-console.log(favouriteFoods);
-favouriteFoods.unshift("Burger");
-console.log(favouriteFoods);
+// let favouriteFoods = ["chicken", "brocolli", "rice"];
+// console.log (favouriteFoods[1]);
+// favouriteFoods.splice(2,1,"biryani");
+// console.log(favouriteFoods);
+// let newJunkFood = favouriteFoods.shift();
+// console.log(newJunkFood)
+// favouriteFoods.push("Whey Protein");
+// console.log(favouriteFoods);
+// // favouriteFoods.unshift("Burger");
+// console.log(favouriteFoods);
 
-let newEmptyArray = [];
-newEmptyArray.pop();
-console.log(newEmptyArray);
+// let newEmptyArray = [];
+// newEmptyArray.pop();
+// console.log(newEmptyArray);
 
-let firstArray = [2,3,4,5];
-firstArray.splice(1,1);
-console.log(firstArray);
+// let firstArray = [2,3,4,5];
+// firstArray.splice(1,1);
+// console.log(firstArray);
 
-let secondArray = ["alpha", "gamma", "delta"];
-//Insert elements by making the second argument 0, which indicates to not remove any elements
-secondArray.splice(1,0,"beta"); 
-console.log(secondArray);
+// let secondArray = ["alpha", "gamma", "delta"];
+// //Insert elements by making the second argument 0, which indicates to not remove any elements
+// secondArray.splice(1,0,"beta"); 
+// console.log(secondArray);
 
-let thirdArray = [10,-10,-5,-3,2,1]; // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-thirdArray.splice(1,3,9,8,7,6,5,4,3);
-console.log(thirdArray);
+// let thirdArray = [10,-10,-5,-3,2,1]; // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// thirdArray.splice(1,3,9,8,7,6,5,4,3);
+// console.log(thirdArray);
+
+// let arr = [1,2,3,4];
+// console.log(arr.length); // 4
+// console.log (arr[arr.length]); // undefined
+// console.log(arr[arr.length-1]); // 4 - this is a nice way to access the last element of an array when you don't know how many elements are inside it.
+
 
